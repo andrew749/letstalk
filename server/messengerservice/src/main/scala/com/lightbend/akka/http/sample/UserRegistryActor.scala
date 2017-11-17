17f8bda2840e42a23141c2a1809d095e7132d7cf
Andrew Codispoti <andrewcod749@gmail.com>
2017-11-17 01:14:59 -0500

    Added Json class to marshall user request data.

--- server/messengerservice/src/main/scala/com/lightbend/akka/http/sample/UserRegistryActor.scala
+++ server/messengerservice/src/main/scala/com/lightbend/akka/http/sample/UserRegistryActor.scala
@@ -1,6 +1,7 @@
 package com.lightbend.akka.http.sample
 
 import akka.actor.{Actor, ActorLogging, Props}
+import akka.event.Logging
 import com.lightbend.akka.http.sample.data_models.UserModel
 
 //#user-case-classes
@@ -21,12 +22,14 @@ class UserRegistryActor extends Actor with ActorLogging {
   def receive: Receive = {
 
     case CreateUser(user) =>
+      log.debug("Got user create message")
       // add user to set
       // TODO: Save to storage with db logic
       users += user
       sender() ! ActionPerformed(s"User ${user.id} created.")
 
     case GetUser(id) =>
+      log.debug("Got user getuser message")
       sender() ! users.find(_.id == id)
 
     // TODO: Delete from storage with db logic
--- server/messengerservice/src/main/scala/com/lightbend/akka/http/sample/routes/MessageRoutes.scala
+++ server/messengerservice/src/main/scala/com/lightbend/akka/http/sample/routes/MessageRoutes.scala
@@ -1,20 +1,34 @@
 package com.lightbend.akka.http.sample.routes
 
 import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.event.Logging
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import com.lightbend.akka.http.sample.{ChatServer, JsonSupport}
-import com.lightbend.akka.http.sample.data_models.IncomingMessagePayload
+import com.lightbend.akka.http.sample.{ChatServer, ChatService, JsonSupport, UserRegistryActor}
+import com.lightbend.akka.http.sample.data_models.{IncomingMessagePayload, Message, NormalUser, UserModel}
 import akka.pattern.ask
 import akka.util.Timeout
+import com.lightbend.akka.http.sample.UserRegistryActor.GetUser
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+import scala.concurrent.Future
+
+case class MessageData(from: String, to: String, payload: String)
 
 trait MessageRoutes extends JsonSupport  {
+
 
   implicit val system: ActorSystem
   implicit val timeout: Timeout
 
   // reference to the actor for the chat server
-  lazy val chatServerActor: ActorRef = system.actorOf( Props[ChatServer] )
+  lazy val chatServerActor: ActorRef = system.actorOf( Props[ChatService] )
+
+  // TODO: make this a public service
+  val userRegistryActor: ActorRef
+
+  private lazy val log = Logging(system, classOf[MessageRoutes])
 
   lazy val messageRoute: Route =
     pathPrefix("messages") {
@@ -24,6 +38,7 @@ trait MessageRoutes extends JsonSupport  {
   lazy val sendMessageRoute: Route =
     pathPrefix("send") {
       post {
+        log.debug("Got message send route")
         entity(as[IncomingMessagePayload]) { payload =>
 
           // send the message to the chat server
@@ -32,11 +47,31 @@ trait MessageRoutes extends JsonSupport  {
           complete(response)
         }
       }
+      get {
+        entity(as[MessageData]) { data: MessageData =>
+          log.debug("Got message")
+
+          val fromUser: Future[UserModel] = (userRegistryActor ? GetUser(data.from)).mapTo[UserModel]
+          val toUser: Future[UserModel] =  (userRegistryActor ? GetUser (data.to)).mapTo[UserModel]
+
+          fromUser foreach  {
+            from => {
+              toUser foreach  {
+                to =>
+                  chatServerActor ! Message(from, to, Some(IncomingMessagePayload(data.payload, System.currentTimeMillis())))
+              }
+            }
+          }
+
+          complete("OK")
+        }
+      }
     }
 
   lazy val getMessagesRoute: Route =
     pathPrefix("get") {
       path(Segment) { userId =>
+        log.debug("Got message get route")
        // TODO: get messages for this user
         ???
       }
