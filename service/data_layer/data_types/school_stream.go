f73247619da363d1a2eca42a1a060da6886cbd92
Andrew Codispoti <andrewcod749@gmail.com>
2018-01-22 23:34:31 -0800

    Fixed a lot of compile errors.

--- service/data_layer/data_types/school_stream.go
+++ service/data_layer/data_types/school_stream.go
@@ -6,20 +6,20 @@ type SchoolStreamError struct {
 	ErrorCode int
 }
 
-func CreateSchoolStream(schoolStream SchoolStream) SchoolStream, error {
-  if err := validateSchoolStream(schoolStream); err != nil {
-    return nil, err
-  }
-  return schoolStream, nil
+func CreateSchoolStream(schoolStream *SchoolStream) (*SchoolStream, error) {
+	if err := validateSchoolStream(schoolStream); err != nil {
+		return nil, err
+	}
+	return schoolStream, nil
 }
 
-func validateSchoolStream(schoolStream SchoolStream) error {
-  switch schoolStream {
-  case "4 Stream":
-  case "8 Stream":
-    return nil
-  }
-  return SchoolStreamError{0}
+func validateSchoolStream(schoolStream *SchoolStream) error {
+	switch *schoolStream {
+	case "4 Stream":
+	case "8 Stream":
+		return nil
+	}
+	return SchoolStreamError{0}
 }
 
 func (sse SchoolStreamError) Error() string {
