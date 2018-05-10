c5e2744e055fbf217e8aff144007b0ee2499192e
Steven Kong <stevendjkong@gmail.com>
2018-05-10 13:01:44 -0400

    Fix request + style about page

--- landing/js/form-submit.js
+++ landing/js/form-submit.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#signupBtn").click(function(){
+    $("#signupBtn").click(function(e){
         if($("#signupForm")[0].checkValidity()) {
             var formData = $("#signupForm").serializeArray();
             var formObj = {};
@@ -12,12 +12,14 @@ $(document).ready(function() {
 
             $.ajax({
                 type: "POST",
+                // PROD: url: "api.hiveapp.org/v1/subscribe_email",
                 url: "v1/subscribe_email",
                 data: JSON.stringify(formObj),
                 success: function(){},
                 dataType: "json",
                 contentType : "application/json"
             });
+            e.preventDefault();
         }
     }); 
 });
