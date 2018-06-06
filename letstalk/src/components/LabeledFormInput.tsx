65adeaa4e268208745a9c2e4e52dc19d6e6b780a
Andrew Codispoti <andrewcod749@gmail.com>
2018-06-05 21:25:57 -0400

    Fix focus management on signup form. (#99)

--- letstalk/src/components/LabeledFormInput.tsx
+++ letstalk/src/components/LabeledFormInput.tsx
@@ -3,27 +3,40 @@ import { View } from 'react-native';
 import { WrappedFieldProps } from 'redux-form';
 import { FormValidationMessage, FormInputProps, FormInput, FormLabel } from 'react-native-elements';
 
-type Props = FormInputProps & WrappedFieldProps & { label: string }
+type Props = FormInputProps & WrappedFieldProps & { label: string, onSubmitEditing?: () => void }
 
-const LabeledFormInput: SFC<Props> = props => {
-  const { label } = props;
-  const { onChange, onBlur, value } = props.input;
-  const { error, touched, warning } = props.meta;
-  return (
-    <View>
-      {label && <FormLabel>{label}</FormLabel>}
-      <FormInput
-        {...props}
-        onBlur={onBlur as () => void} // Thanks jhang (type hack to make this typecheck)
-        onChangeText={onChange}
-        value={value}
-      />
-      {touched && (
-        (error && <FormValidationMessage>{error}</FormValidationMessage>) ||
-        (warning && <FormValidationMessage>{warning}</FormValidationMessage>))}
-    </View>
-  );
+class LabeledFormInput extends React.Component<Props> {
+  private elementRef: React.Ref<FormInput>;
+  constructor(props: Props) {
+    super(props);
+    this.elementRef = React.createRef();
+  }
+  focus() {
+    // @ts-ignore
+    this.elementRef.current.focus();
+  }
+  render() {
+    const props = this.props;
+    const { label } = props;
+    const { onChange, onBlur, value } = props.input;
+    const { error, touched, warning } = props.meta;
+    return (
+      <View>
+        {label && <FormLabel>{label}</FormLabel>}
+        <FormInput
+          {...props}
+          ref={this.elementRef}
+          onBlur={onBlur as () => void} // Thanks jhang (type hack to make this typecheck)
+          onChangeText={onChange}
+          value={value}
+          onSubmitEditing={props.onSubmitEditing}
+        />
+        {touched && (
+          (error && <FormValidationMessage>{error}</FormValidationMessage>) ||
+          (warning && <FormValidationMessage>{warning}</FormValidationMessage>))}
+        </View>
+      );
+    }
 };
 
 export default LabeledFormInput;
-
