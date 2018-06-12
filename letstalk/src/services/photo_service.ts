68e384bc2a330d09af5ed9e034a90bb443607db2
Andrew Codispoti <andrewcod749@gmail.com>
2018-06-11 20:29:12 -0400

    Include client side image compression and change aspect ratio to 1:1.

--- letstalk/src/services/photo_service.ts
+++ letstalk/src/services/photo_service.ts
@@ -26,7 +26,8 @@ export class PhotoServiceImpl implements PhotoService {
 
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [1, 1],
+      quality: 0.2,
       base64: true,
     });
 
