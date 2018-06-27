3106a54fe8f05b268d5809eab7b7fc65e76f8177
Andrew Codispoti <andrewcod749@gmail.com>
2018-06-27 07:19:59 -0400

    Update deploy to build frontend. Updated build frontend to not reinstall node every time.

--- infra/build_frontend.sh
+++ infra/build_frontend.sh
@@ -2,7 +2,11 @@
 
 # Build files for the static site frontend.
 
-# Get the apprpriate nodejs and gulp
+FRONTEND_DIRECTORY="../landing"
+
+# Get the apprpriate nodejs and gulp if not installed
+NODE_INSTALLED=$(apt-cache policy nodejs | grep  Installed: | sed s/Installed://)
+if [[ -z $NODE_INSTALLED ]]; then
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 apt-get install npm
@@ -10,9 +14,12 @@ npm install -g gulp
 
 # setup nodejs environment
 ln -s /usr/bin/nodejs /usr/bin/node
+fi
 
-# install dependencies
-npm --prefix ../landing install
+if [[ -z $(which gulp) ]]; then
+  # install dependencies
+  npm --prefix $FRONTEND_DIRECTORY install
+fi
 
 # build
-gulp --cwd ../landing
+gulp --cwd $FRONTEND_DIRECTORY
--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ./build_prod.sh
+./build_frontend.sh
 ./prod.sh
