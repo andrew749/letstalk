c4bb087393f05ca087a90c3209a689e837b847f8
Andrew Codispoti <andrewcod749@gmail.com>
2018-06-27 09:44:33 -0400

    Moved provision server script to infra. Added more logging in this script.

--- infra/provision_server.sh
+++ infra/provision_server.sh
@@ -3,8 +3,8 @@
 # To be run from AWS cloud in an EC2 instance
 
 # Group to create for server administration (including running the server)
-ADMINGROUP="serveradmingrp"
-ADMINUSER="serveradmin"
+ADMINGROUP="server_grp"
+ADMINUSER="server"
 
 # directories for the app
 HOME=/var/app/letstalk
@@ -16,9 +16,7 @@ create_admin_group() {
 }
 
 create_admin_user() {
-    # create user  without home directory that cant login
-    sudo useradd -M $ADMINUSER
-    sudo usermod -L $ADMINUSER
+    sudo useradd $ADMINUSER
     sudo usermod -aG $ADMINGROUP $ADMINUSER
 }
 
@@ -32,11 +30,23 @@ setup_docker() {
     docker swarm init
 }
 
+generate_ssh() {
+  su $ADMINUSER
+  ssh-keygen
+  echo "BEGIN PUBLIC KEY"
+  cat ~/.ssh/id_rsa.pub
+  echo "END PUBLIC KEY"
+}
+
 
 # start of actual program
 create_admin_group
 create_admin_user
 install_dependencies
+generate_ssh
 setup_docker
+
+echo "\033[92mAdding source code.\033[0m"
+git clone git@github.com:andrew749/letstalk.git
 
 echo "\033[91mRemember to manually add secrets to this server\033[0m"
