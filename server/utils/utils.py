e4006743d0b0e24edbdefa4b9b0bdf7669693d98
Andrew Codispoti <andrewcod749@gmail.com>
2018-06-26 23:51:58 -0400

    Removed unused template method.

--- server/utils/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def template_file(file_path: str, out_path: str, fill_in_dict={}):
-    with open(file_path, 'r') as input_file:
-        with open(out_path, 'w') as out_file:
-            data = input_file.read()
-            formatted_data = data.format(fill_in_dict)
-            out_file.write(formatted_data)
