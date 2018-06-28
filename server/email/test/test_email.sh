e7cfae854febff6c0b663bd36065de93f36ad078
Andrew Codispoti <andrewcod749@gmail.com>
2018-06-28 18:50:43 -0400

    Bad command.

--- server/email/test/test_email.sh
+++ server/email/test/test_email.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
- 
-SECRETS_PATH=server/secrets.json go run server/email/test/test_email.go --subscribe --forgot_password
+
+SECRETS_PATH=server/secrets.json go run server/email/test/test_email.go --subscribe --forgotPass
