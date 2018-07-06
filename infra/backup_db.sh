89a3245e0e15442bd2541fad01f44b6878351151
Andrew Codispoti <andrewcod749@gmail.com>
2018-07-05 23:37:13 -0400

    wip

--- infra/backup_db.sh
+++ infra/backup_db.sh
@@ -7,4 +7,4 @@ OUT_DIR="/var"
 
 PASSWORD=$(jq -r '.db_pass' $DIRECTORY/server/secrets.json)
 
-mysql -h $DB  -u $USERNAME -p$PASSWORD -d > $OUT_DIR/$(date +%Y-%m-%dT%::z).db.sql
+mysqldump -h $DB  -u $USERNAME -p$PASSWORD > $OUT_DIR/$(date +%Y-%m-%dT%::z).db.sql
