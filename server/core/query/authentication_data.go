84c72b7e85e6d5bfbcd9b043a98e9651143cae26
Andrew Codispoti <andrewcod749@gmail.com>
2018-07-05 22:34:29 -0400

    Wrote integration tests to check changes.

---
+++ server/core/query/authentication_data.go
@@ -0,0 +1,15 @@
+package query
+
+import (
+	"letstalk/server/data"
+
+	"github.com/jinzhu/gorm"
+)
+
+func GetHashForUser(db *gorm.DB, userId int) (*string, error) {
+	var authData data.AuthenticationData
+	if err := db.Where(&data.AuthenticationData{UserId: userId}).First(&authData).Error; err != nil {
+		return nil, err
+	}
+	return &authData.PasswordHash, nil
+}
