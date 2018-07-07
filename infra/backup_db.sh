f32c2e8b05060b7cf57f512d63da59fb45d0c036
Andrew Codispoti <andrewcod749@gmail.com>
2018-07-07 07:56:18 -0400

    Db snapshot script (#140)
    Added infra db backup script

---
+++ infra/backup_db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIRECTORY="/var/app/letstalk"
+DB="letstalk.cobpajnfrzy1.us-east-1.rds.amazonaws.com"
+USERNAME="hiveadmin"
+OUT_DIR="/var"
+DB_TABLE="letstalk"
+
+PASSWORD=$(jq -r '.db_pass' $DIRECTORY/server/secrets.json)
+
+mysqldump -h $DB -u $USERNAME -p$PASSWORD $DB_TABLE > $OUT_DIR/$(date +%Y-%m-%dT%T).db.sql
