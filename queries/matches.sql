2ffd0115754d6752dcbebda0b93afb3389703d5f
Andrew Codispoti <andrewcod749@gmail.com>
2018-07-08 08:10:23 -0400

    Added userful queries for our current db. (#144)

---
+++ queries/matches.sql
@@ -0,0 +1,8 @@
+select distinct
+  users.first_name,
+  name,
+  mentors.first_name
+from users
+inner join request_matchings on asker=user_id
+inner join credentials on credential_id=credentials.id
+inner join users mentors on answerer=mentors.user_id;
