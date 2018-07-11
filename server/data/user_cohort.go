9773fdf70619a79f441aea6c818dffac797e5266
Wojtek Swiderski <wswiders@uwaterloo.ca>
2018-07-11 11:52:17 -0400

    Fix cohort getting bug

--- server/data/user_cohort.go
+++ server/data/user_cohort.go
@@ -3,6 +3,6 @@ package data
 type UserCohort struct {
 	User     User    `gorm:"foreignkey:UserId"`
 	UserId   int     `json:"userId" gorm:"not null;primary_key;auto_increment:false"`
-	Cohort   *Cohort `gorm:"foreignkey:CohortId"`
+	Cohort   *Cohort `gorm:"foreignkey:CohortId;association_foreignkey:CohortId"`
 	CohortId int     `json:"cohortId" gorm:"not null"`
 }
