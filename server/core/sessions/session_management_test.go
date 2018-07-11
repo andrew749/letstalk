d6adade9366b96d049b3420d5d315b37d5c70291
Andrew Codispoti <andrewcod749@gmail.com>
2018-07-11 00:10:58 -0400

    Added tests for session management write through cache.

--- server/core/sessions/session_management_test.go
+++ server/core/sessions/session_management_test.go
@@ -1,8 +1,11 @@
 package sessions
 
 import (
+	"letstalk/server/core/test"
 	"testing"
+	"time"
 
+	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -38,4 +41,44 @@ func TestCreateAndGetSessionBySessionId(t *testing.T) {
 
 	session, _ := sm.GetSessionForSessionId(*session1.SessionId)
 	assert.Equal(t, session1, session)
+}
+
+func TestDatabaseSessionStore(t *testing.T) {
+	tests := []test.Test{
+		test.Test{
+			Test: func(db *gorm.DB) {
+				sd := CreateDBSessionStore(db)
+				sm := CreateCompositeSessionManager(sd)
+				session1, _ := sm.CreateNewSessionForUserId(1, nil)
+				session, _ := sm.GetSessionForSessionId(*session1.SessionId)
+				assert.Equal(t, session1.SessionId, session.SessionId)
+			},
+			TestName: "Test Database session creation",
+		},
+	}
+	test.RunTestsWithDb(tests)
+}
+
+func TestWriteThroughCache(t *testing.T) {
+	tests := []test.Test{
+		test.Test{
+			Test: func(db *gorm.DB) {
+				sd := CreateDBSessionStore(db)
+				sessionData, err := CreateSessionData(1, nil, time.Now())
+				assert.NoError(t, err)
+
+				sd.AddNewSession(sessionData)
+				ss := CreateInMemorySessionStore()
+				sm := CreateCompositeSessionManager(ss, sd)
+				session, err := sm.GetSessionForSessionId(*sessionData.SessionId)
+				assert.NoError(t, err)
+				assert.NotNil(t, session)
+				session2, err := ss.GetSessionForSessionId(*sessionData.SessionId)
+				assert.NoError(t, err)
+				assert.NotNil(t, session2)
+			},
+			TestName: "Test Database session creation",
+		},
+	}
+	test.RunTestsWithDb(tests)
 }
