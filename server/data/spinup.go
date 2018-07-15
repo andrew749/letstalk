da40ce12a29b1fa98d72f179b2b01e0253b99eea
Andrew Codispoti <andrewcod749@gmail.com>
2018-07-14 22:04:06 -0400

    Added migration framework to project. (#158)
    * Added migration framework to project.
    
    * Actually handle first error.

--- server/data/spinup.go
+++ server/data/spinup.go
@@ -2,28 +2,47 @@ package data
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/romana/rlog"
+	"gopkg.in/gormigrate.v1"
 )
 
 func migrateDB(db *gorm.DB) {
-	db.AutoMigrate(&AuthenticationData{})
-	db.AutoMigrate(&Cohort{})
-	PopulateCohort(db)
-	db.AutoMigrate(&User{})
-	db.AutoMigrate(&Session{})
-	db.AutoMigrate(&UserVector{})
-	db.AutoMigrate(&UserCohort{})
-	db.AutoMigrate(&UserAdditionalData{})
-	db.AutoMigrate(&NotificationToken{})
-	db.AutoMigrate(&ExternalAuthData{})
-	db.AutoMigrate(&FbAuthToken{})
-	db.AutoMigrate(&Matching{})
-	db.AutoMigrate(&RequestMatching{})
-	db.AutoMigrate(&Credential{})
-	db.AutoMigrate(&UserCredential{})
-	db.AutoMigrate(&UserCredentialRequest{})
-	db.AutoMigrate(&Subscriber{})
-	db.AutoMigrate(&ForgotPasswordId{})
-	db.AutoMigrate(&MeetingConfirmation{})
+	m := gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
+		{
+			ID: "1",
+			Migrate: func(tx *gorm.DB) error {
+				tx.AutoMigrate(&AuthenticationData{})
+				tx.AutoMigrate(&Cohort{})
+				PopulateCohort(tx)
+				tx.AutoMigrate(&User{})
+				tx.AutoMigrate(&Session{})
+				tx.AutoMigrate(&UserVector{})
+				tx.AutoMigrate(&UserCohort{})
+				tx.AutoMigrate(&UserAdditionalData{})
+				tx.AutoMigrate(&NotificationToken{})
+				tx.AutoMigrate(&ExternalAuthData{})
+				tx.AutoMigrate(&FbAuthToken{})
+				tx.AutoMigrate(&Matching{})
+				tx.AutoMigrate(&RequestMatching{})
+				tx.AutoMigrate(&Credential{})
+				tx.AutoMigrate(&UserCredential{})
+				tx.AutoMigrate(&UserCredentialRequest{})
+				tx.AutoMigrate(&Subscriber{})
+				tx.AutoMigrate(&ForgotPasswordId{})
+				tx.AutoMigrate(&MeetingConfirmation{})
+				return tx.Error
+			},
+			Rollback: func(tx *gorm.DB) error {
+				return nil
+			},
+		},
+	})
+
+	if err := m.Migrate(); err != nil {
+		rlog.Errorf("Could not migrate: %v", err)
+	}
+
+	rlog.Infof("Succesfully ran migration")
 }
 
 func CreateDB(db *gorm.DB) {
