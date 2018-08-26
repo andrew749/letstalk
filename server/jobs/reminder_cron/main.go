1513821195456698f6314574a16f6755e836eb56
Andrew Codispoti <andrewcod749@gmail.com>
2018-08-25 22:31:25 -0400

    got stuff working with sqs.

--- server/jobs/reminder_cron/main.go
+++ server/jobs/reminder_cron/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"letstalk/server/core/notifications"
+	"letstalk/server/core/query"
 	"letstalk/server/data"
 	"letstalk/server/utility"
 	"time"
@@ -15,35 +15,50 @@ import (
 
 func getTimeXDaysAgo(days int) string {
 	now := time.Now()
-	res := now.Add(time.Duration(days*24) * time.Hour)
-	return res.Format("%x")
+	res := now.Add(-time.Duration(days*24) * time.Hour)
+	return res.Format("2006-01-02")
 }
 
 const (
-	NUM_DAYS_TILL_NOTIFICATION = 30
+	NUM_DAYS_TILL_NOTIFICATION = 1
 )
 
 var (
 	runId = flag.String("runId", time.Now().String(), "Id to mark these notifications with")
 )
 
+const (
+	MENTOR_TITLE   = `Reconnect with your mentee!!`
+	MENTOR_MESSAGE = `Its been almost a month since you last connected with
+	your mentee. You should reach out to see how they are doing`
+	MENTEE_TITLE   = `Reconnect with your mentor!!`
+	MENTEE_MESSAGE = `Its been almost a month since you last connected with
+		your mentor. You should reach out if you have any questions or
+		just want to reconnect!`
+)
+
 func main() {
-	flag.Parse()
+	utility.Bootstrap()
+
 	// find all relationships that are a month old and send out reconnection notifications
 	err := utility.RunWithDb(func(tx *gorm.DB) error {
 		// find all matchings a month ago
 		// TODO: find a more efficient way of doing this.
 		// this is going to pull a lot of data in memory
+		startTime := getTimeXDaysAgo(NUM_DAYS_TILL_NOTIFICATION)
 		rows, err := tx.
+			Table("matchings").
 			Select("matchings.id, s.id").
-			Where("created_at < ?", getTimeXDaysAgo(NUM_DAYS_TILL_NOTIFICATION)).
-			Joins("left join sent_monthly_notifications s on s.matching_id=id").
+			Where("matchings.created_at < ?", startTime).
+			Joins("left join sent_monthly_notifications s on s.matching_id=matchings.id").
 			Rows()
 
 		if err != nil {
 			rlog.Error(err)
 			return err
 		}
+
+		rlog.Debugf("Finding matchings from before %s", startTime)
 
 		// don't preallocate the whole array size to save some mem
 		// only keep entries with null id on sent_monthly_notifications
@@ -62,56 +77,76 @@ func main() {
 				panic(err)
 			}
 
-			if tempResStruct.SendNotificationId != nil {
+			if tempResStruct.SendNotificationId == nil {
 				toSend = append(toSend, tempResStruct.MatchingId)
-				rlog.Debug("Adding Notification for matching with id %d", tempResStruct.MatchingId)
+				rlog.Debugf("Adding Notification for matching with id %d", tempResStruct.MatchingId)
 			} else {
-				rlog.Debug("Monthly Notification for matching with id %d already sent", tempResStruct.MatchingId)
+				rlog.Debugf("Monthly Notification for matching with id %d already sent", tempResStruct.MatchingId)
 			}
 
 		}
 
 		for _, matchingId := range toSend {
-			// send out notifications for each of these
+			rlog.Debugf("Sending notification for matching with id %d", matchingId)
+			// send out notifications for each of these matchings
+
+			// get the matching data object
 			matching, err := data.GetMatchingWithId(tx, matchingId)
 			if err != nil {
 				rlog.Error(err)
 				raven.CaptureError(err, nil)
 				continue
 			}
-			mentorMessage := fmt.Sprintf("Its been almost a month since you last connected with your mentee. You should reach out to see how they are doing")
-			menteeMessage := fmt.Sprint("Its been almost a month since you last connected with your mentor. You should reach out if you have any questions or just want to reconnect!")
 
+			// get the user data for users involved in the matching
+			mentor, err := query.GetUserProfileById(tx, matching.Mentor)
+			if err != nil {
+				rlog.Error(err)
+				continue
+			}
+
+			mentee, err := query.GetUserProfileById(tx, matching.Mentee)
+			if err != nil {
+				rlog.Error(err)
+				continue
+			}
+
+			// create notification for mentor
 			mentorErr := notifications.CreateAdHocNotification(
 				tx,
 				matching.Mentor,
-				"Reconnect with your mentee!!",
-				mentorMessage,
+				MENTOR_TITLE,
+				MENTOR_MESSAGE,
 				nil,
 				"first_month_notification_mentor.html",
 				map[string]string{
-					"name": "Andrew",
+					"name": mentee.FirstName,
 				},
 			)
 
+			// create notification for mentee
 			menteeErr := notifications.CreateAdHocNotification(
 				tx,
 				matching.Mentor,
-				"Reconnect with your mentor!!",
-				menteeMessage,
+				MENTEE_TITLE,
+				MENTEE_MESSAGE,
 				nil,
 				"first_month_notification_mentee.html",
 				map[string]string{
-					"name": "Wojtek",
+					"name": mentor.FirstName,
 				},
 			)
+			// in reality, either both of these should fail or neither since we're
+			// just putting in an sqs queue.
+			// For now, not modelling with more granular control (i.e. was the mentor/mentee notification sent)
 			if mentorErr != nil {
 				rlog.Error(mentorErr)
 			}
 			if menteeErr != nil {
 				rlog.Error(menteeErr)
 			}
-			// if not error
+
+			// if not error, mark this notification as being sent by a batch job.
 			if err := data.SentOutMonthlyNotification(tx, matchingId, *runId); err != nil {
 				rlog.Error(err)
 			}
--- server/utility/common.go
+++ server/utility/common.go
@@ -1,6 +1,8 @@
 package utility
 
 import (
+	"letstalk/server/core/secrets"
+
 	"github.com/namsral/flag"
 )
 
@@ -9,9 +11,19 @@ var (
 	dbUser = flag.String("db_user", "", "mySQL user")
 	dbPass = flag.String("db_pass", "", "mySQL password")
 	dbAddr = flag.String("db_addr", "", "address of the database connection")
+
+	secretsPath = flag.String("secrets_path", "~/secrets.json", "path to secrets.json")
 )
 
 // Flags to get es credentials
 var (
 	esAddr = flag.String("es_addr", "", "address of the elasticsearch connection")
 )
+
+// Methods to run before a client is initialized
+func Bootstrap() {
+	flag.Parse()
+
+	// bootstrap secrets from local file
+	secrets.LoadSecrets(*secretsPath)
+}
