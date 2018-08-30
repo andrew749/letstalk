3ea587bcec37fe8c731a88475f40962c3545acef
Adam Klen <adam.j.klen@gmail.com>
2018-08-30 12:37:49 -0400

    Add verify email static page

--- landing/js/verify-email.js
+++ landing/js/verify-email.js
@@ -1,50 +1,43 @@
 $(document).ready(function(){
 
-  var baseUrl = "https://api.hiveapp.org/v1";
+  var baseUrl = "http://localhost:80/v1";
 
   function getVerifyEmailRequestId() {
     var searchParams = new URLSearchParams(window.location.search);
     return searchParams.get('requestId');
   }
 
-  function setSuccessMessage(title, text) {
+  function setSuccessMessage(msg) {
+    $("#message-container").html($('<div/>', {class: 'alert alert-success'}).text(msg));
   }
 
-  function setErrorMessage(title, text) {
+  function setErrorMessage(msg) {
+    $("#message-container").html($('<div/>', {class: 'alert alert-danger'}).text(msg).appendTo("#message-container"));
   }
 
   function submitEmailVerifyRequest(requestId) {
-    console.log("sending email verify request");
+    console.log("sending email verify request for id: ", requestId);
     $.post( baseUrl + "/verify_email", JSON.stringify({
-      "requestId": requestId,
+      "id": requestId,
     })).done( function( data ) {
       // on success change ui
       console.log("Successfully submitted verify email request: ", data);
-      setSuccessMessage("Email successfully verified")
+      setSuccessMessage("Successfully verified email.")
     }).fail(function( data ) {
       console.log("Failed to verify email: ", data);
-      setErrorMessage("Error verifying email")
+      setErrorMessage("Error verifying email. Please make sure to use the most recent email link you received.")
     });
   }
 
   function handleRequest() {
-    let requestId = getVerifyEmailRequestId();
-    if (requestId === undefined || requestId === "") {
-      setErrorMessage("Error: invalid email verification link");
+    var requestId = getVerifyEmailRequestId();
+    if (!requestId) {
+      console.error("missing request id in query string");
+      setErrorMessage("Invalid email verification link.");
       return;
     }
-    submitEmailVerifyRequest();
+    submitEmailVerifyRequest(requestId);
   }
 
   handleRequest();
-
-  function badPasswordHandler() {
-    console.log("Bad password");
-    $("#message-container").html($('<div/>', {class: 'alert alert-danger'}).text("Failed to change password.").appendTo("#message-container"));
-  }
-
-  function passwordChangeHandler() {
-    console.log("password changed");
-    $("#message-container").html($('<div/>', {class: 'alert alert-success'}).text("Successfully changed password."));
-  }
 });
