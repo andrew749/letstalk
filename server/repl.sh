d9a2acaa8d4f8e58f350cc711dbae4235de39596
Andrew Codispoti <andrewcod749@gmail.com>
2018-08-31 13:01:23 -0400

    Added gore dependency.

--- server/repl.sh
+++ server/repl.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-SECRETS_PATH="secrets.json"
-# get options
-export DB_PASS=$(jq -r '.db_pass' ${SECRETS_PATH})
-export DB_ADDR=$(jq -r '.db_addr' ${SECRETS_PATH})
-export DB_USER=$(jq -r '.db_user' ${SECRETS_PATH})
+docker run -it letstalk_webapp bash <<EOF
+  SECRETS_PATH="secrets.json"
+  # get options
+  export DB_PASS=$(jq -r '.db_pass' ${SECRETS_PATH})
+  export DB_ADDR=$(jq -r '.db_addr' ${SECRETS_PATH})
+  export DB_USER=$(jq -r '.db_user' ${SECRETS_PATH})
 
-gore
-
+  # run the repl
+  gore <<EOF
+    :import letstalk/server/utility
+  EOF
+EOF
