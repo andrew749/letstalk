01b53bea65b79e8a0fd2850367624ebdd7e424f2
Andrew Codispoti <andrewcod749@gmail.com>
2018-08-31 17:57:00 -0400

    Script to send test adhoc notifications.

---
+++ server/jobs/test_adhoc_notification/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"letstalk/server/core/notifications"
+	"letstalk/server/data"
+	"letstalk/server/utility"
+
+	"github.com/namsral/flag"
+)
+
+var (
+	recipient      *int
+	message        *string
+	title          *string
+	thumbnail      *string
+	templatePath   *string
+	templateParams *string
+)
+
+func main() {
+	db, err := utility.GetDB()
+	if err != nil {
+		panic(err)
+	}
+
+	flag.IntVar(recipient, "recipient", 0, "Enter the userId of the recipient")
+	flag.StringVar(message, "message", "", "Enter the message to send")
+	flag.StringVar(title, "title", "", "Enter the title")
+	flag.StringVar(thumbnail, "thumbnail", "", "Enter the thumbnail path")
+	flag.StringVar(templatePath, "templatePath", "", "Enter the template path")
+	flag.StringVar(templateParams, "templateParams", "", "Enter the template params")
+	flag.Parse()
+	var params map[string]string
+	if err := json.Unmarshal([]byte(*templateParams), &params); err != nil {
+		panic(err)
+	}
+
+	notifications.CreateAdHocNotification(db, data.TUserID(*recipient), *title, *message, thumbnail, *templatePath, params)
+}
