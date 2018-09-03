387135bd2261bc0ab9f6c9e79b9a2f5d82325f9c
Adam Klen <adam.j.klen@gmail.com>
2018-09-03 17:56:58 -0400

    Add data model

---
+++ server/data/connection.go
@@ -0,0 +1,46 @@
+package data
+
+import (
+	"time"
+)
+
+type TConnectionID EntID
+
+type Connection struct {
+	ConnectionId TConnectionID `gorm:"not null;primary_key;auto_increment"`
+	UserOne      *User         `gorm:"foreignkey:UserOneId"`
+	UserOneId    TUserID
+	UserTwo      *User         `gorm:"foreignkey:UserTwoId"`
+	UserTwoId    TUserID
+	CreatedAt    time.Time     `gorm:"not null"`
+	UpdatedAt    time.Time
+	DeletedAt    time.Time
+	AcceptedAt   time.Time // Null until user two accepts.
+}
+
+type IntentType string
+const (
+	INTENT_TYPE_SEARCH IntentType = "search"
+	INTENT_TYPE_REC_GENERAL IntentType = "recommendation_general"
+	INTENT_TYPE_REC_COHORT IntentType = "recommendation_cohort"
+	INTENT_TYPE_REFERRAL IntentType = "referral"
+)
+
+type ConnectionIntent struct {
+	ConnectionId  TConnectionID `gorm:"not null;primary_key"`
+	Connection    *Connection   `gorm:"foreignkey:Connection"`
+	Type          IntentType    `gorm:"not null;size:100"`
+	SearchedTrait *string       `gorm:"type:text"` // Only applies to "search" type
+	Referrer      *User         `gorm:"foreignkey:ReferrerId"`
+	ReferrerId    *TUserID // Only applies to "referral" type
+}
+
+type Mentorship struct {
+	ConnectionId TConnectionID `gorm:"not null;primary_key"`
+	Connection   *Connection   `gorm:"foreignkey:Connection"`
+	MentorUser   *User         `gorm:"foreignkey:MentorUserId"`
+	MentorUserId TUserID
+	CreatedAt    time.Time     `gorm:"not null"`
+	UpdatedAt    time.Time
+	DeletedAt    time.Time
+}
