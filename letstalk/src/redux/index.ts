ff9d785bfd8606329ddbc28f95c3cce5fdf6f547
Wojtek Swiderski <wswiders@uwaterloo.ca>
2018-09-19 22:46:13 -0700

    Remove credentials from frontend

--- letstalk/src/redux/index.ts
+++ letstalk/src/redux/index.ts
@@ -4,28 +4,16 @@ import { toastReducer as toast } from 'react-native-redux-toast';
 
 import { reducer as bootstrap, State as BootstrapState } from './bootstrap/reducer';
 import { reducer as cohorts, State as CohortsState } from './cohorts/reducer';
-import { reducer as credentials, State as CredentialsState } from './credentials/reducer';
 import { reducer as onboarding, State as OnboardingState } from './onboarding/reducer';
 import { reducer as matchProfile, State as MatchProfileState } from './match-profile/reducer';
 import { reducer as notifications, State as NotificationsState } from './notifications/reducer';
 import { reducer as profile, State as ProfileState } from './profile/reducer';
 import { reducer as userSearch, State as UserSearchState } from './user-search/reducer';
-import {
-  reducer as credentialRequests,
-  State as CredentialRequestsState,
-} from './credential-requests/reducer';
-import {
-  reducer as credentialOptions,
-  State as CredentialOptionsState,
-} from './credential-options/reducer';
 import { reducer as searchBar, State as SearchBarState } from './search-bar/reducer';
 
 export interface RootState {
   bootstrap: BootstrapState;
   cohorts: CohortsState;
-  credentialOptions: CredentialOptionsState;
-  credentialRequests: CredentialRequestsState;
-  credentials: CredentialsState;
   form: FormState;
   onboarding: OnboardingState;
   matchProfile: MatchProfileState;
@@ -38,9 +26,6 @@ export interface RootState {
 const reducers = {
   bootstrap,
   cohorts,
-  credentialOptions,
-  credentialRequests,
-  credentials,
   form,
   toast,
   onboarding,
