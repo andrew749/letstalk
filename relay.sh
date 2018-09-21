36e77a17c60c8dbcb86ccb536c00811cd1eabc37
Andrew Codispoti <andrewcod749@gmail.com>
2018-09-21 00:00:55 -0700

    update echo

--- relay.sh
+++ relay.sh
@@ -8,8 +8,8 @@
 # set in the /etc/ssh/sshd_config
 
 
-read -p "Enter username: " username
-read -p "Enter port to forward to: " port
+read -p "Enter username to use for ssh: " username
+read -p "Enter local port to forward: " port
 ssh -g -R 10123:localhost:$port $username@hiveapp.org
 
 echo "Now you can access your port $port at hiveapp.org:10123! MAKE SURE TO KEEP THIS CONNECTION OPEN"
