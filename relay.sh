dadf6ed58c25cf6f41f706072068ea987778b491
Andrew Codispoti <andrewcod749@gmail.com>
2018-09-24 20:22:57 -0700

    Made the relay more generic and configurable. (#220)

--- relay.sh
+++ relay.sh
@@ -7,9 +7,11 @@
 # GatewayPorts yes
 # set in the /etc/ssh/sshd_config
 
+# $port must be in the range 10100-10200
 
 read -p "Enter username to use for ssh: " username
-read -p "Enter local port to forward: " port
-ssh -g -R 10123:localhost:$port $username@hiveapp.org
+read -p "Enter local port to forward(on this computer): " port
+read -p "Select a port to use on the remote server[10100,10200]: " portRemote
+ssh -g -R $portRemote:localhost:$port $username@hiveapp.org
 
-echo "Now you can access your port $port at hiveapp.org:10123! MAKE SURE TO KEEP THIS CONNECTION OPEN"
+echo "Now you can access your port $port at hiveapp.org:$portRemote! MAKE SURE TO KEEP THIS CONNECTION OPEN"
