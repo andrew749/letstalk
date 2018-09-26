20407583a55b2b4a387e1ca605bde2e67b6149e1
Andrew Codispoti <andrewcod749@gmail.com>
2018-09-26 00:47:54 -0700

    Removed verification logic.

--- server/core/meeting/meeting.go
+++ server/core/meeting/meeting.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/getsentry/raven-go"
 	"github.com/jinzhu/gorm"
-	"github.com/romana/rlog"
 )
 
 // PostMeetingConfirmation lets users confirm that a scheduled meeting occurred.
@@ -30,9 +29,7 @@ func PostMeetingConfirmation(c *ctx.Context) errs.Error {
 		return errs.NewRequestError("Could not find user")
 	}
 
-	// TODO: find and confirm existing meeting with this user, if exists.
-
-	matchingObj, err := query.GetMatchingByUserIds(c.Db, authUser.UserId, matchedUser.UserId)
+	matchingObj, err := query.GetConnectionDetailsUndirected(c.Db, authUser.UserId, matchedUser.UserId)
 	if err != nil {
 		return errs.NewDbError(err)
 	}
@@ -52,12 +49,10 @@ func PostMeetingConfirmation(c *ctx.Context) errs.Error {
 			CreatedAt:  time.Now(),
 			AcceptedAt: &now,
 		}
-		secret := "SECRET"
-		message := "I'd like to connect with you."
 		intent := data.ConnectionIntent{
 			Type:          data.INTENT_TYPE_SCAN_CODE,
-			SearchedTrait: &secret,
-			Message:       &message,
+			SearchedTrait: nil,
+			Message:       nil,
 		}
 		dbErr := c.WithinTx(func(tx *gorm.DB) error {
 			if err := tx.Create(&connection).Error; err != nil {
@@ -72,41 +67,8 @@ func PostMeetingConfirmation(c *ctx.Context) errs.Error {
 		if dbErr != nil {
 			return errs.NewDbError(dbErr)
 		}
-
-	} else {
-		isFirstMeeting := matchingObj.State == data.MATCHING_STATE_UNVERIFIED
-
-		// Store a confirmation of the meeting for future reference.
-		conf := &data.MeetingConfirmation{
-			MatchingId: matchingObj.ID,
-		}
-
-		dbErr := c.WithinTx(func(tx *gorm.DB) error {
-			if err := tx.Model(&data.MeetingConfirmation{}).Create(conf).Error; err != nil {
-				return err
-			}
-			// Verify the matching if this is the first confirmed meeting.
-			if isFirstMeeting {
-				if err := saveVerifiedMatch(tx, *matchingObj); err != nil {
-					return err
-				}
-			}
-			return nil
-		})
-		if dbErr != nil {
-			return errs.NewDbError(err)
-		}
-
-		if isFirstMeeting {
-			// Also send a notification now that the match is verified.
-			go func() {
-				if err := sendMatchVerifiedNotifications(c, authUser, matchedUser); err != nil {
-					rlog.Errorf("Error sending verified match notification: %s", err)
-					raven.CaptureError(err, nil)
-				}
-			}()
-		}
 	}
+
 	c.Result = input
 
 	return nil
