a6eaeb9787df461fcb520010544b436f12208d9a
Andrew Codispoti <andrewcod749@gmail.com>
2018-09-30 15:22:37 -0700

    Removed file for notification tokens.

--- server/data/notification_tokens.go
+++
@@ -1,7 +0,0 @@
-package data
-
-type DEPRECATED_NotificationToken struct {
-	SessionId string `gorm:"not null;primary_key;size:190"`
-	Token     string `gorm:"not null;primary_key;size:190"`
-	Service   string
-}
