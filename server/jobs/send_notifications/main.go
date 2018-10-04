37342d6553bcac76a8044e4f0a9d8a58154325fb
Andrew Codispoti <andrewcod749@gmail.com>
2018-10-03 22:15:19 -0700

    ques que ce

---
+++ server/jobs/send_notifications/main.go
@@ -0,0 +1,13 @@
+package main
+
+// A command line tool to send batches of an adhoc notification to a group of users.
+
+import "github.com/namsral/flag"
+
+var (
+	userId = flag.String()
+)
+
+func main() {
+
+}
