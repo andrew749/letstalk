f762f35d81e1fac25b571c82a32a79f7b2bd9ab5
Andrew Codispoti <andrewcod749@gmail.com>
2018-10-07 18:07:57 -0700

    Only use deeplink if one exist.

--- letstalk/src/components/NotificationBody.tsx
+++ letstalk/src/components/NotificationBody.tsx
@@ -10,8 +10,6 @@ import {
   StyleSheet,
 } from 'react-native';
 import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
-import NotificationService from '../services/notification-service';
-import { Linking } from 'expo';
 
 const styles = StyleSheet.create({
   root: {
