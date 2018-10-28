c675e0d8781e5eb0f34c7513c7c2c856a55c6f76
Andrew Codispoti <andrewcod749@gmail.com>
2018-10-27 20:56:22 -0700

    Updated development file to rebuild static assets on run of dev. This also syncs the files back to your local filesystem.

--- dev.sh
+++ dev.sh
@@ -5,4 +5,12 @@
 # setup latest git hooks
 cp hooks/* .git/hooks
 
+# if the environment image was already built, then rebuilt web assets
+if [ $(docker images -q letstalk_webapp) -a $(docker images -q letstalk_env) ];
+then
+  # essentially mount our local directory web build directory into this container so we get the
+  # build artifacts from docker
+  docker run -it -v "$(pwd)"/server/web/dist:/go/src/letstalk/server/web/dist letstalk_env yarn --cwd web build
+fi
+
 docker-compose -f 'docker-compose.yml' -f 'docker-compose-debug.yml' up
