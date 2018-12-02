d7aa76dc1328a2d40db44628949962e9ed965d86
Andrew Codispoti <andrewcod749@gmail.com>
2018-12-02 12:36:39 -0800

    Fixed directory mounting in run_in_env command.

--- server/run_in_env.sh
+++ server/run_in_env.sh
@@ -4,4 +4,9 @@ set -e
 set -x
 
 EXEC_COMMAND="source source_secrets.sh secrets.json && $@"
-docker run --network="$DB_NET" -v "$(pwd):/go/src/letstalk/server" -it letstalk_env bash -c "$EXEC_COMMAND"
+if [ -n "$PROD" ]; then
+    VOL_DIR="/var/app/letstalk/server"
+else
+    VOL_DIR=$(pwd)
+fi
+docker run --network="$DB_NET" -v "$VOL_DIR:/go/src/letstalk/server" -it letstalk_env bash -c "$EXEC_COMMAND"
