3936f315c8dc2ac4fe7ee31acca57b96b9130d59
Andrew Codispoti <andrewcod749@gmail.com>
2018-12-02 10:48:20 -0800

    Added configurable db network for run_in_env script.

--- server/run_in_env.sh
+++ server/run_in_env.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run --network="letstalk_db_net" -v "$(pwd):/go/src/letstalk/server" -it letstalk_env bash -c "source source_secrets.sh secrets.json && $@"
+set -e
+set -x
+
+docker run --network="$DB_NET" -v "$(pwd):/go/src/letstalk/server" -it letstalk_env bash -c "source source_secrets.sh secrets.json && $@"
