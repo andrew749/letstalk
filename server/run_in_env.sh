b186b634016e4891e9a0ef4c5347f0d34c2e92b9
Andrew Codispoti <andrewcod749@gmail.com>
2018-12-02 13:56:07 -0800

    Updated crontab with new logging direcotry. fixed env script to have optional tty.

--- server/run_in_env.sh
+++ server/run_in_env.sh
@@ -3,10 +3,20 @@
 set -e
 set -x
 
+# Flags:
+# - PROD: whether to use the cwd or the prod working directory
+# - TTY: whether to allow tty input to docker
+
 EXEC_COMMAND="source source_secrets.sh secrets.json && $@"
 if [ -n "$PROD" ]; then
     VOL_DIR="/var/app/letstalk/server"
 else
     VOL_DIR=$(pwd)
 fi
-docker run --network="$DB_NET" -v "$VOL_DIR:/go/src/letstalk/server" -it letstalk_env bash -c "$EXEC_COMMAND"
+
+TTY_ARGS=""
+if [ -n "$TTY" ]; then
+    TTY_ARGS="-it"
+fi
+
+docker run $TTY_ARGS --network="$DB_NET" -v "$VOL_DIR:/go/src/letstalk/server" letstalk_env bash -c "$EXEC_COMMAND"
