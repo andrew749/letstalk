3bf589bb0ad36cf1802c2e42cc5e95b6d30f8a2b
Andrew Codispoti <andrewcod749@gmail.com>
2018-12-02 12:25:48 -0800

    Fix path pointing to for crontab.

--- server/run_in_env.sh
+++ server/run_in_env.sh
@@ -3,4 +3,5 @@
 set -e
 set -x
 
-docker run --network="$DB_NET" -v "$(pwd):/go/src/letstalk/server" -it letstalk_env bash -c "source source_secrets.sh secrets.json && $@"
+EXEC_COMMAND="source source_secrets.sh secrets.json && $@"
+docker run --network="$DB_NET" -v "$(pwd):/go/src/letstalk/server" -it letstalk_env bash -c "$EXEC_COMMAND"
