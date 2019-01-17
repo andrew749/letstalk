1dc47cea56170bdf757b3ce1b2755cf74703bd74
Andrew Codispoti <andrewcod749@gmail.com>
2019-01-16 19:35:46 -0500

    Added collapsible element to card.

--- letstalk/src/components/InformationCard.tsx
+++ letstalk/src/components/InformationCard.tsx
@@ -4,9 +4,11 @@ import Colors from '../services/colors';
 import Card from '../components/Card';
 import { Linking } from 'expo';
 import navService from '../services/navigation-service';
+import Collapsible from 'react-native-collapsible';
+
 
 enum InformationCardType {
-    CLUB_DAY = 'information-card-club-day-visibility',
+    CLUB_DAY = 'information-card-club-day-visibilitya',
     PROFILE_FILL_CALL_TO_ACTION = 'profile-fill-cta',
 }
 
@@ -70,26 +72,44 @@ class InformationCard extends React.Component<Props, State> {
 }
 
 interface ClubDayProps { }
+interface ClubDayState { 
+    isCollapsed: boolean;
+}
 
-export const ClubDayInformationCard: React.SFC<ClubDayProps> = props => {
-    return (
-        <InformationCard title="Welcome to the Hive!" cardType={InformationCardType.CLUB_DAY}>
-            <Text style={[styles.textSection]}>
-                Matches will be coming out in the next couple of weeks. Stay tuned!
+export class ClubDayInformationCard extends React.Component<ClubDayProps, ClubDayState> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            isCollapsed: true,
+        };
+    }
+
+    render() {
+        const moreButton = (this.state.isCollapsed) ? <TouchableOpacity onPress={() => this.setState({ isCollapsed: false })}>
+            <Text>Show more</Text>
+        </TouchableOpacity> : undefined;
+        return (
+            <InformationCard title="Welcome to the Hive!" cardType={InformationCardType.CLUB_DAY}>
+                <Text style={[styles.textSection]}>
+                    Matches will be coming out in the next couple of weeks. Stay tuned!
             </Text>
-            <Text style={styles.textSection}>
-                In the meanwhile, feel free to search for people you might be interested in connecting with. For example:
+                <Text style={styles.textSection}>
+                    In the meanwhile, feel free to search for people you might be interested in connecting with. For example:
             </Text>
-            <Text style={[styles.points]}>- Meet other people in your cohort</Text>
-            <Text style={[styles.points]}>- Ask for tips from a person who worked at your dream company</Text>
-            <Text style={[styles.points]}>- Find someone who went on an exchange term</Text>
-            <Text style={[styles.textSection, { marginTop: 20 }]}>Expand your horizons. Grow your network.</Text>
-            <Text style={[styles.signature]}>The Hive Team</Text>
-            <View style={styles.imageContainer}>
-                <Image style={styles.imageStyle} source={require('../img/logo_android.png')} />
-            </View>
-        </InformationCard>
-    );
+            {moreButton}
+                <Collapsible collapsed={this.state.isCollapsed}>
+                    <Text style={[styles.points]}>- Meet other people in your cohort</Text>
+                    <Text style={[styles.points]}>- Ask for tips from a person who worked at your dream company</Text>
+                    <Text style={[styles.points]}>- Find someone who went on an exchange term</Text>
+                    <Text style={[styles.textSection, { marginTop: 20 }]}>Expand your horizons. Grow your network.</Text>
+                    <Text style={[styles.signature]}>The Hive Team</Text>
+                    <View style={styles.imageContainer}>
+                        <Image style={styles.imageStyle} source={require('../img/logo_android.png')} />
+                    </View>
+                </Collapsible>
+            </InformationCard>
+        );
+    }
 }
 
 interface ProfileFillCallToActionProps { }
@@ -108,6 +128,9 @@ export const ProfileFillCallToAction: React.SFC<ProfileFillCallToActionProps> = 
 const PADDING_TEXT_SECTION = 10;
 
 const styles = StyleSheet.create({
+    moreButton: {
+        color: '#BDBDBD',
+    },
     cancelButtonContainer: {
         justifyContent: 'flex-end',
         alignItems: 'flex-end',
