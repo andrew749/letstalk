92e8670cb79b5a34e7eea02f5f12d51cb0823fa5
Andrew Codispoti <andrewcod749@gmail.com>
2019-01-29 00:29:13 -0500

    Handle expo notification errors (#315)
    * Added jobmine job to check sent notifications and cleanup stale tokens.
    
    * Updated so test cases pass.
    
    * Changed name of notification check functino to reflect semantics.
    
    * Added tests and more code review changes.
    
    * Fix bad type cast.
    
    * Add more debugging and fault tolerance.

--- server/jobmine_jobs/specs.go
+++ server/jobmine_jobs/specs.go
@@ -2,6 +2,7 @@ package jobmine_jobs
 
 import (
 	"letstalk/server/jobmine"
+	"letstalk/server/jobmine_jobs/notification_status_checker"
 	"letstalk/server/jobmine_jobs/remind_meetup_job"
 	"letstalk/server/jobmine_jobs/remind_onboard_job"
 	"letstalk/server/jobmine_jobs/test_job"
@@ -13,9 +14,10 @@ import (
 // stored in the database to the actual code that should get executed.
 var Jobs = jobmine.JobSpecStore{
 	JobSpecs: map[jobmine.JobType]jobmine.JobSpec{
-		test_job.TestJob:                              test_job.TestJobSpec,
-		remind_onboard_job.REMIND_ONBOARD_JOB:         remind_onboard_job.ReminderJobSpec,
-		remind_meetup_job.REMIND_MEETUP_JOB:           remind_meetup_job.ReminderJobSpec,
-		welcome_back_email_job.WELCOME_BACK_EMAIL_JOB: welcome_back_email_job.ReminderJobSpec,
+		test_job.TestJob:                                            test_job.TestJobSpec,
+		remind_onboard_job.REMIND_ONBOARD_JOB:                       remind_onboard_job.ReminderJobSpec,
+		remind_meetup_job.REMIND_MEETUP_JOB:                         remind_meetup_job.ReminderJobSpec,
+		welcome_back_email_job.WELCOME_BACK_EMAIL_JOB:               welcome_back_email_job.ReminderJobSpec,
+		notification_status_checker.NOTIFICATION_STATUS_CHECKER_JOB: notification_status_checker.NotificationStatusChecker,
 	},
 }
