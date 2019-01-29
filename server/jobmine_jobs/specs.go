e3d3e64313558c63669b48f983f94c424d576b58
Wojtek Swiderski <wswiders@uwaterloo.ca>
2019-01-28 21:46:25 -0500

    Added email job

--- server/jobmine_jobs/specs.go
+++ server/jobmine_jobs/specs.go
@@ -5,6 +5,7 @@ import (
 	"letstalk/server/jobmine_jobs/remind_meetup_job"
 	"letstalk/server/jobmine_jobs/remind_onboard_job"
 	"letstalk/server/jobmine_jobs/test_job"
+	"letstalk/server/jobmine_jobs/welcome_back_email_job"
 )
 
 // IMPORTANT: JobType and the key need to be identical here.
@@ -12,8 +13,9 @@ import (
 // stored in the database to the actual code that should get executed.
 var Jobs = jobmine.JobSpecStore{
 	JobSpecs: map[jobmine.JobType]jobmine.JobSpec{
-		test_job.TestJob:                      test_job.TestJobSpec,
-		remind_onboard_job.REMIND_ONBOARD_JOB: remind_onboard_job.ReminderJobSpec,
-		remind_meetup_job.REMIND_MEETUP_JOB:   remind_meetup_job.ReminderJobSpec,
+		test_job.TestJob:                              test_job.TestJobSpec,
+		remind_onboard_job.REMIND_ONBOARD_JOB:         remind_onboard_job.ReminderJobSpec,
+		remind_meetup_job.REMIND_MEETUP_JOB:           remind_meetup_job.ReminderJobSpec,
+		welcome_back_email_job.WELCOME_BACK_EMAIL_JOB: welcome_back_email_job.ReminderJobSpec,
 	},
 }
