d7f401ffd3eec2da531e5c74480052de5d9a7ccd
Andrew Codispoti <andrewcod749@gmail.com>
2019-03-03 02:30:34 -0500

    removed config.

--- server/web/src/config.js
+++ server/web/src/config.js
@@ -1,4 +1,4 @@
-//export const serverUrl = 'https://api.hiveapp.org';
-export const serverUrl = 'http://localhost'; // For dev
+export const serverUrl = 'https://api.hiveapp.org';
+//export const serverUrl = 'http://192.168.0.179'; // For dev
 export const hiveDeepLinkRoot = 'hive:/';
 //export const hiveDeepLinkRoot = 'exp://192.168.0.179:19000/--'; // For dev
