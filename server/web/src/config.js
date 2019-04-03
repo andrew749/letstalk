1674010e4e15c05cdc539ea954b2586a1c4210d7
Andrew Codispoti <andrewcod749@gmail.com>
2019-04-03 11:09:11 -0400

    Ac fix links admin page (#361)
    * Updated admin page to use config file.
    
    * Updated config to fix api to use v1

--- server/web/src/config.js
+++ server/web/src/config.js
@@ -3,4 +3,10 @@ export const serverUrl = 'https://api.hiveapp.org';
 export const hiveDeepLinkRoot = 'hive:/';
 //export const hiveDeepLinkRoot = 'exp://192.168.0.179:19000/--'; // For dev
 
+export const apiUrl = `${serverUrl}/v1`;
+
+// api v1 endpoints
+export const loginUrl = `${apiUrl}/login`;
+
+// admin endpoints
 export const mentorshipUrl = `${serverUrl}/admin/mentorship`;
