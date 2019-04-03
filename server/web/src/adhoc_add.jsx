4aaf6556ae177eaeee62391555e6383448ee19c9
Andrew Codispoti <andrewcod749@gmail.com>
2019-04-02 23:28:17 -0400

    Added user deletion tool.

--- server/web/src/adhoc_add.jsx
+++ server/web/src/adhoc_add.jsx
@@ -4,6 +4,7 @@ import { withCookies } from 'react-cookie';
 import { Form, Button, Alert, Container, Row, Col } from 'react-bootstrap';
 import './scss/notification_console.scss';
 import { createMentorshipFromEmails } from './admin_api_controller.js';
+import { createMentorshipFromEmails, deleteUser as deleteUserApi } from './admin_api_controller.js';
 import {connect} from 'react-redux';
 
 class AdminPanel extends React.Component {
@@ -18,11 +19,20 @@ class AdminPanel extends React.Component {
         mentorEmail: undefined,
         menteeEmail: undefined,
         error: undefined
-      }
+      },
+      userDeletionToolModel: {
+        userId: undefined,
+        firstName: undefined,
+        lastName: undefined,
+        email: undefined,
+        error: undefined
+      },
     }
 
     this.adhocMatchingToolChange = this.onChange.bind(this, 'adhocMatchingToolModel');
+    this.userDeletionToolChange = this.onChange.bind(this, 'userDeletionToolModel');
     this.createConnection = this.createConnection.bind(this);
+    this.deleteUser = this.deleteUser.bind(this);
   }
 
   onUpdateData(newNotificationData) {
@@ -49,18 +59,39 @@ class AdminPanel extends React.Component {
     const mentorEmail = this.state.adhocMatchingToolModel.mentorEmail;
     const menteeEmail = this.state.adhocMatchingToolModel.menteeEmail;
     createMentorshipFromEmails(mentorEmail, menteeEmail)
-      .then(response => response.json())
       .then((data) => {
-        if (data.Error) {
-          throw new Error(data.Error.message)
-        }
         // handle success response
-        console.log(data);
         this.setState({ adhocMatchingToolModel: { error: undefined } });
         console.log("Successfully created mentorship.");
       }).catch(err => {
         this.setState({ adhocMatchingToolModel: { error: err.message } });
         console.warn("Failed to create mentorship");
+      });
+  }
+
+  deleteUser(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    const model = this.state.userDeletionToolModel;
+    if (!(model.email && model.firstName && model.lastName && model.userId )) {
+      console.log("Not enough fields filled out");
+      this.setState({
+        userDeletionToolModel: {
+          ...this.state.userDeletionToolModel,
+          error: "Missing required field"
+        }
+      })
+      return;
+    }
+    deleteUserApi(model.userId, model.firstName, model.lastName, model.email)
+      .then((data) => {
+        // handle success response
+        console.log(data);
+        this.setState({ userDeletionToolModel: { error: undefined } });
+        console.log("Successfully deleted user");
+      }).catch(err => {
+        this.setState({ userDeletionToolModel: { error: err.message } });
+        console.warn("Failed to delete user");
       });
   }
 
@@ -84,6 +115,9 @@ class AdminPanel extends React.Component {
     const { cookies } = this.props;
     const adhocMatchingToolError = (this.state.adhocMatchingToolModel.error)
       ? <Alert key="adhocMatchingToolResponse" variant="danger">{this.state.adhocMatchingToolModel.error}</Alert>
+      : undefined;
+    const deleteToolError = (this.state.userDeletionToolModel.error)
+      ? <Alert key="deleteToolResponse" variant="danger">{this.state.userDeletionToolModel.error}</Alert>
       : undefined;
     return (
         <Container>
@@ -115,6 +149,34 @@ class AdminPanel extends React.Component {
               </Form>
             </Col>
           </Row>
+          <hr/>
+          <Row>
+            <h1>
+              {"User Deletion tool"}
+            </h1>
+          </Row>
+          <Row>
+            <p>
+              Enter the details of the user to delete.
+            </p>
+          </Row>
+          <Row>
+            <Col style={{ paddingLeft: 0, paddingRight: 0 }} lg="12">
+              <Form onSubmit={this.deleteUser}>
+                <Form.Group controlId="formDeleteUser">
+                  <Form.Control size="lg" type="number" name="userId" placeholder="User Id" onChange={this.userDeletionToolChange} />
+                  <Form.Control size="lg" type="email" name="email" placeholder="Email" onChange={this.userDeletionToolChange} />
+                  <Form.Control size="lg" type="text" name="firstName" placeholder="First Name" onChange={this.userDeletionToolChange} />
+                  <Form.Control size="lg" type="text" name="lastName" placeholder="Last Name" onChange={this.userDeletionToolChange} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Delete
+              </Button>
+                {deleteToolError}
+              </Form>
+            </Col>
+          </Row>
+          <hr/>
         </Container>
     );
   }
