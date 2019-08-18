c0339cd2be6d02568cb1755d880813f7cac6c055
Andrew Codispoti <acodispo@acodispo-mn1.linkedin.biz>
2019-08-18 14:42:27 -0700

    Fixed bug in check user script.

--- scripts/check_user_list.sh
+++ scripts/check_user_list.sh
@@ -7,6 +7,6 @@ while IFS= read -r line;
 do
 user=${line%$'\r'}
 url="https://api.hiveapp.org/admin/user_exists?email=$user"
-curl -s -X GET $url -H 'sessionId: $sessionId' | jq -r '.Result' | xargs -I{} echo "${user}={}";
+curl -s -X GET $url -H "sessionId: $sessionId" | jq -r '.Result' | xargs -I{} echo "${user}={}";
 
 done < $filename
